f93c41726b7fa8425264ebd7abfa8e90dc338706
Madhavi <ggm@softwareag.com>
2023-03-05 11:34:16 +0530

    node fetch test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require("fs");
+const fetch = require('node-fetch');
 const { connected } = require('process');
 
 async function checkFileExistence(path) {
