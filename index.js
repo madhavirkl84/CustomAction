3e99f5380c5c1d1d132864f3b9aae95c8e1e3b5f
Madhavi <ggm@softwareag.com>
2023-03-14 16:46:47 +0530

    checke

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function checkFileExistence(path) {
 
     //console.log(context);
     try {
-        const octokit = octokit = new Octokit({
+        const octokit = new Octokit({
             auth: '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'
           });
 
